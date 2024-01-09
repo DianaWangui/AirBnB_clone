f46961559683b994cac88087246901ac8c36b3cc
DianaWangui <dianawanguikt048@gmail.com>
2024-01-09 06:11:49 +0300

    updating the base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,2 +1,27 @@
 #!/usr/bin/python3
+"""A model that defines all common attributes."""
+import uuid
+from datetime import datetime
 
+
+class BaseModel:
+    """instantiating public instance attributes."""
+    id = str(uuid.uuid4())
+    created_at = datetime.now()
+    updated_at = datetime.now()
+
+    def __str__(self):
+        """ print the class_name id and dict in a specified format."""
+        return("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        """update the public instance updated_at with current time."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """return a dictionary containing all keys/values of __dict__"""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__clas__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
