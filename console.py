3d7dcdc6aedb024d9415454a9c3c46bd85861dea
DianaWangui <dianawanguikt048@gmail.com>
2024-01-10 14:55:31 +0300

    update console and file

--- console.py
+++ console.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """A console model that is the entry point of cmd interpreter."""
 import cmd
-
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """A command line interpreter class."""
+    classes = { "BaseModel" : BaseModel
+            }
 
     prompt = "(hbnb)"
     def do_quit(self, command):
@@ -25,9 +27,15 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, command):
         """Instantiating a new create method to save in JSON."""
-        self.object = arg[0]
-        if self.object is None:
-            print("**class name missind **")
-        elif 
+        args = command.split()
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in self.classes:
+            print("** class doesn't exist **")
+        else:
+            new_instance = self.classes[arg[0]]()
+            new_instance.save()
+            print(new_instance.id)
+
 if __name__== '__main__':
     HBNBCommand().cmdloop()
