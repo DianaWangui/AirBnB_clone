099d22d11b7d2fd154135809737ba2a31a4b7a97
bo1atit0 <shorungbebolatito@gmail.com>
2024-01-11 06:20:05 +0100

    task 9 modules

--- console.py
+++ console.py
@@ -3,6 +3,12 @@
 import cmd
 from models.base_model import BaseModel
 from models.user import User #task8
+# task10
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 
 class HBNBCommand(cmd.Cmd):
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,6 +8,12 @@ import os
 import json
 from models.base_model import BaseModel
 from models.user import User # task8
+# task10
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 
 class FileStorage:
