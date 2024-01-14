2afc60498bdb9232abd996ed22cbb4f172dbf381
bo1atit0 <shorungbebolatito@gmail.com>
2024-01-14 09:04:33 +0100

    pycode errors

--- tests/test_models/test_engine/test_filestorage.py
+++ tests/test_models/test_engine/test_filestorage.py
@@ -19,79 +19,76 @@ from models.user import User
 
 
 class Test_filestorage(unittest.TestCase):
-  """
-  Testing the filestorage class and it's methods
-  """
+    """
+    Testing the filestorage class and it's methods
+    """
+    def setUp(self):
+        """
+        create a FileStorage object to be tested
+        """
+        self.file_storage = FileStorage()
 
-  def setUp(self):
-    """
-    create a FileStorage object to be tested
-    """
-    self.file_storage = FileStorage()
+    def tearDown(self):
+        """
+        delete the FileStorage object
+        """
+        del self.file_storage
 
-  def tearDown(self):
-    """
-    delete the FileStorage object
-    """
-    del self.file_storage
+    def test_all(self):
+        """
+        test the all() method
+        """
+        self.assertIsInstance(self.file_storage.all(), dict)
+        # self.assertEqual(self.file_storage.all(),
+        # self.file_storage.FileStorage.__objects)
 
-  def test_all(self):
-    """
-    test the all() method
-    """
-    self.assertIsInstance(self.file_storage.all(), dict)
-    #self.assertEqual(self.file_storage.all(), self.file_storage.FileStorage.__objects)
-
-  def test_new(self):
-    """
-    test the new() method
-    """
-    # test BaseModel
-    new_obj = BaseModel()
-    self.file_storage.new(new_obj)
-    key = "{}.{}".format(new_obj.__class__.__name__, new_obj.id)
-    new_dict = self.file_storage.all()
-    self.assertIn(key, new_dict)
-    #test Amenity
-    new_obj = Amenity()
-    self.file_storage.new(new_obj)
-    key = "{}.{}".format(new_obj.__class__.__name__, new_obj.id)
-    new_dict = self.file_storage.all()
-    self.assertIn(key, new_dict)
-    #test City
-    new_obj = City()
-    self.file_storage.new(new_obj)
-    key = "{}.{}".format(new_obj.__class__.__name__, new_obj.id)
-    new_dict = self.file_storage.all()
-    self.assertIn(key, new_dict)
-    #test Place
-    new_obj = Place()
-    self.file_storage.new(new_obj)
-    key = "{}.{}".format(new_obj.__class__.__name__, new_obj.id)
-    new_dict = self.file_storage.all()
-    self.assertIn(key, new_dict)
-    #test Review
-    new_obj = Review()
-    self.file_storage.new(new_obj)
-    key = "{}.{}".format(new_obj.__class__.__name__, new_obj.id)
-    new_dict = self.file_storage.all()
-    self.assertIn(key, new_dict)
-    #test State
-    new_obj = State()
-    self.file_storage.new(new_obj)
-    key = "{}.{}".format(new_obj.__class__.__name__, new_obj.id)
-    new_dict = self.file_storage.all()
-    self.assertIn(key, new_dict)
-    #test User
-    new_obj = User()
-    self.file_storage.new(new_obj)
-    key = "{}.{}".format(new_obj.__class__.__name__, new_obj.id)
-    new_dict = self.file_storage.all()
-    self.assertIn(key, new_dict)
-
-
-
+    def test_new(self):
+        """
+        test the new() method
+        """
+        # test BaseModel
+        new_obj = BaseModel()
+        self.file_storage.new(new_obj)
+        key = "{}.{}".format(new_obj.__class__.__name__, new_obj.id)
+        new_dict = self.file_storage.all()
+        self.assertIn(key, new_dict)
+        # test Amenity
+        new_obj = Amenity()
+        self.file_storage.new(new_obj)
+        key = "{}.{}".format(new_obj.__class__.__name__, new_obj.id)
+        new_dict = self.file_storage.all()
+        self.assertIn(key, new_dict)
+        # test City
+        new_obj = City()
+        self.file_storage.new(new_obj)
+        key = "{}.{}".format(new_obj.__class__.__name__, new_obj.id)
+        new_dict = self.file_storage.all()
+        self.assertIn(key, new_dict)
+        # test Place
+        new_obj = Place()
+        self.file_storage.new(new_obj)
+        key = "{}.{}".format(new_obj.__class__.__name__, new_obj.id)
+        new_dict = self.file_storage.all()
+        self.assertIn(key, new_dict)
+        # test Review
+        new_obj = Review()
+        self.file_storage.new(new_obj)
+        key = "{}.{}".format(new_obj.__class__.__name__, new_obj.id)
+        new_dict = self.file_storage.all()
+        self.assertIn(key, new_dict)
+        # test State
+        new_obj = State()
+        self.file_storage.new(new_obj)
+        key = "{}.{}".format(new_obj.__class__.__name__, new_obj.id)
+        new_dict = self.file_storage.all()
+        self.assertIn(key, new_dict)
+        # test User
+        new_obj = User()
+        self.file_storage.new(new_obj)
+        key = "{}.{}".format(new_obj.__class__.__name__, new_obj.id)
+        new_dict = self.file_storage.all()
+        self.assertIn(key, new_dict)
 
 
 if '__name__ ' == '__main__':
-  unittest.main()
+    unittest.main()
